ab2e9ba4bfa2c14e320f49aa2c9160822326ee42
parmeet2 <parmeet@damacloid.com>
2025-07-04 12:48:37 -0400

    Adding Cloudnary for file upload

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,13 +1,18 @@
+// To upload files to Cloudinary
+// This allows us to handle file uploads in our Express routes easily.
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 console.log("Cloudinary configuration loaded");
+
+// Configure Cloudinary with environment variables
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Create a Cloudinary storage instance for file uploads
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
